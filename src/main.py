bdf6c90f99fffe2883610e1e0476225889b6bfa6
Nikolai Pakhtusov <chesterlanduk@gmail.com>
2023-04-07 11:08:31 +0200

    Added Allow-origins * header

--- src/main.py
+++ src/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.routers import process
 from src.routers import user
 from src.settings import DEBUG
 
 app = FastAPI(debug=DEBUG)
+if DEBUG:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 app.include_router(process.router)
 app.include_router(user.router)
