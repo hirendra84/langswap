25ca022e3379c4b353df3f2e11a14bf9d7e13a6f
Valeriy Grachev <vg@finery.tech>
2023-05-08 15:13:17 +0100

    Fix: CORS traefik settings
    Add: debug CORS traefik settings
    
    Add: debug CORS traefik settings
    
    Add: debug CORS traefik settings
    
    Add: debug CORS traefik settings
    
    Add: debug CORS traefik settings
    
    Add: debug CORS traefik settings

--- src/main.py
+++ src/main.py
@@ -6,14 +6,6 @@ from src.routers import user
 from src.settings import DEBUG
 
 app = FastAPI(debug=DEBUG)
-if DEBUG:
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=["*"],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
 
 app.include_router(process.router)
 app.include_router(user.router)
