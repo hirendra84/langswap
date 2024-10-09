f686c6bd60c8d94a3f1bb7a0d462b8b58bd4f784
MilanaShhanukova <milana.shxanukova15@gmail.com>
2024-10-09 17:54:32 +0300

    add the translation service with cohere, mitigate deepl

--- src/ml/translation_service/translator_client.py
+++ src/ml/translation_service/translator_client.py
@@ -1,71 +1,95 @@
 from abc import ABC
-import deepl
+import json
 import torch
 from tqdm import tqdm
-from seamless_communication.inference import Translator
 
 from src.utils.ml_processing.lang2code_mapper import map_language_to_code
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+import torch
+
 
 class TranslatorClient(ABC):
 
-    def __init__(self, key: str):
-        ...
+    def __init__(self, device: str): ...
 
-    def translate(self, sentences: list[str], source_lang: str, target_lang: str) -> list[str]:
-        ...
+    def translate(
+        self, sentences: list[str], source_lang: str, target_lang: str
+    ) -> list[str]: ...
 
 
-class DeepLClient(TranslatorClient):
-    _client: deepl.Translator
+class HugTranslationClient(TranslatorClient):
+    def __init__(
+        self,
+        device="cuda",
+        path_to_model="/app/models_weights/cohereforai-23/cohere/",
+        quantization_config=None,
+    ):
+        super().__init__(device)
 
-    def __init__(self, key: str):
-        super().__init__(key)
-        self._client = deepl.Translator(key)
+        if quantization_config is None:
+            quantization_config = BitsAndBytesConfig(load_in_8bit=True)
+        self.device = device
 
-    def translate(self, sentences: list[str], source_lang: str, target_lang: str) -> list[str]:
-        def _fix_target_language(language: str):
-            if language == 'en':
-                return 'en-US'
-            return language
-        target_lang = _fix_target_language(target_lang)
-        try:
-            translated_sentences = self._client.translate_text(sentences, source_lang=source_lang, target_lang=target_lang)
-        except AttributeError:
-            print()
-        if isinstance(translated_sentences, deepl.TextResult):
-            translated_sentences = [translated_sentences]
-
-        return [r.text for r in translated_sentences]
-
-class SeamlessClient(TranslatorClient):
-    def __init__(self, key: str):
-        super().__init__(key)
-        model_name = "seamlessM4T_v2_large"
-        vocoder_name = "vocoder_v2"
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-        self.translator = Translator(
-            model_name,
-            vocoder_name,
-            device=device,
-            dtype=torch.float16,
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            path_to_model, local_files_only=True, device_map=device
         )
-    
-    def translate(self, sentences: list[str], source_lang: str, target_lang: str) -> list[str]:
-        source_lang = map_language_to_code(source_lang, "seamless")
-        target_lang = map_language_to_code(target_lang, "seamless")
-        
+        self.model = AutoModelForCausalLM.from_pretrained(
+            path_to_model,
+            local_files_only=True,
+            device_map=device,
+            torch_dtype=torch.float16,
+            quantization_config=quantization_config,
+        )
+
+        # self.tokenizer = AutoTokenizer.from_pretrained("CohereForAI/aya-23-8B", device_map=device, token="***REDACTED-HF-TOKEN***")
+        # self.model = AutoModelForCausalLM.from_pretrained("CohereForAI/aya-23-8B", local_files_only=True, device_map=device,
+        #                                                 torch_dtype=torch.float16, quantization_config=quantization_config, token="***REDACTED-HF-TOKEN***")
+
+    def translate_sent(self, input_text: str, input_lang: str, output_lang: str) -> str:
+        messages = [
+            {
+                "role": "system",
+                "content": f"You are a translation assistant. Your task is to translate the text provided in the user's input from the source language to the target language. You will respond only with the translation of the text in the target language in json format with, one field 'translated_text'.",
+            },
+            {
+                "role": "user",
+                "content": f'source_language: "{input_lang}", target_language: "{output_lang}", text: "{input_text}", ',
+            },
+        ]
+        input_ids = self.tokenizer.apply_chat_template(
+            messages, tokenize=True, add_generation_prompt=True, return_tensors="pt"
+        )
+
+        gen_tokens = self.model.generate(
+            input_ids,
+            max_new_tokens=len(input_text),
+            do_sample=True,
+            temperature=0.3,
+        )
+
+        gen_text = self.tokenizer.decode(gen_tokens[0], skip_special_tokens=True).split(
+            "<|CHATBOT_TOKEN|>"
+        )[1]
+        if "{" in gen_text and "}" in gen_text:
+            return json.loads(gen_text)["translated_text"]
+        try:
+            gen_text += "}"
+            return json.loads(gen_text)["translated_text"]
+        except:
+            return gen_text
+
+    def translate(
+        self, sentences: list[str], source_lang: str, target_lang: str
+    ) -> list[str]:
+        source_lang = map_language_to_code(source_lang, "cohere")
+
+        target_lang = map_language_to_code(target_lang, "cohere")
+
         translations = []
         for sent in tqdm(sentences):
-            translated_sent = ""
-            translated_s, _ = self.translator.predict(
-                    input=sent,
-                    task_str="t2st",
-                    tgt_lang=target_lang,
-                    src_lang=source_lang,
-                )
-
-            translated_sent = str(translated_s[0])
+            translated_sent = self.translate_sent(
+                sent, input_lang=source_lang, output_lang=target_lang
+            )
 
             translations.append(translated_sent)
         return translations
