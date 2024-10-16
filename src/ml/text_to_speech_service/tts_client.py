fbdfafefe2efd441cd58ed15b7323c80f05c4a76
MilanaShhanukova <milana.shxanukova15@gmail.com>
2024-10-16 15:49:33 +0300

    add the tts part

--- src/ml/text_to_speech_service/tts_client.py
+++ src/ml/text_to_speech_service/tts_client.py
@@ -1,37 +1,32 @@
+import sys
 from abc import ABC
 
+from src.pipeline_models.models import TranslatedTextedSegment
 from tqdm.auto import tqdm
 
-from src.pipeline_models.models import TranslatedTextedSegment
+sys.path.append("/app/coqui")
 
+import os
 import sys
 
-sys.path.append("/app/coqui-ai-TTS")
-
-from TTS.api import TTS
-import os
+import torch
+import torchaudio
+from coqui.TTS.api import TTS
 from src.file_repository import FileRepository
 from src.utils.ml_processing.lang2code_mapper import map_language_to_code
 
-import torchaudio
-import os
-import torch
-
-import sys
-
-sys.path.append("/app/OpenVoice")
-
-from openvoice import se_extractor
-from openvoice.api import ToneColorConverter
+sys.path.append("/app/voice_conv")
 
 from pydub import AudioSegment
+from voice_conv.openvoice import se_extractor
+from voice_conv.openvoice.api import ToneColorConverter
 
 
 def add_pauses(audio_path: str, num_sec=2):
     audio, sr = torchaudio.load(audio_path)
 
-    pause_start = torch.zeros((1, sr * num_sec))
-    pause_end = torch.zeros((1, sr * num_sec))
+    pause_start = torch.zeros((1, sr*num_sec))
+    pause_end = torch.zeros((1, sr*num_sec))
 
     audio = torch.cat([pause_start, audio, pause_end], dim=1)
 
@@ -40,24 +35,26 @@ def add_pauses(audio_path: str, num_sec=2):
 
 class TTSClient(ABC):
 
-    def __init__(self): ...
+    def __init__(self):
+        ...
 
-    def clone_voice(self, voice_path: str, voice_descr: str = "", voice_name=""): ...
+    def clone_voice(self, voice_path: str, voice_descr: str = '', voice_name = '' ):
+        ...
 
-    def generate_audio(self, text: str, source_audio_file: str, save_path: str): ...
-
-    def tts_pipelene(
-        self, data: list[TranslatedTextedSegment], output_folder: str
-    ) -> list[TranslatedTextedSegment]: ...
+    def generate_audio(self, text: str, source_audio_file: str, save_path: str):
+        ...
+        
+    def tts_pipelene(self, data: list[TranslatedTextedSegment], output_folder: str) -> list[TranslatedTextedSegment]:
+        ...
 
 
 class VoiceToneConverter:
-    def __init__(self, ckpt_converter_folder: str, device="cpu"):
+    def __init__(self,
+                ckpt_converter_folder: str,
+                device="cpu"):
         self.ckpt_converter_folder = ckpt_converter_folder
         config_path = os.path.join(self.ckpt_converter_folder, "converter/config.json")
-        checkpoint_path = os.path.join(
-            self.ckpt_converter_folder, "converter/checkpoint.pth"
-        )
+        checkpoint_path = os.path.join(self.ckpt_converter_folder, "converter/checkpoint.pth")
 
         self.tone_color_converter = ToneColorConverter(config_path, device=device)
         self.tone_color_converter.load_ckpt(checkpoint_path)
@@ -69,94 +66,70 @@ class VoiceToneConverter:
     def merge_enhanced(self, video_translation):
         combined_audio = AudioSegment.empty()
 
-        for idx, segment in enumerate(
-            tqdm(
-                video_translation.translated_texts,
-                desc="Merge enhanced pipeline.",
-                leave=True,
-            )
-        ):
+        for idx, segment in enumerate(tqdm(video_translation.translated_texts, desc='Merge enhanced pipeline.', leave=True)):
             audio_segment = AudioSegment.from_file(segment.source_file)
             combined_audio += audio_segment
 
             save_path = video_translation.background_audio["vocals.wav"]
-            combined_audio.export(
-                save_path.replace("vocals", "vocals_enhanced"), format="wav"
-            )
+            combined_audio.export(save_path.replace("vocals", "vocals_enhanced"), format="wav")
 
-    def voice_conversion_pipeline(self, video_translation, temp_folder, source_lang):
+    def voice_conversion_pipeline(self,
+                            video_translation,
+                            temp_folder,
+                            source_lang):
 
         self.merge_enhanced(video_translation)
-        clean_audio_speaker = video_translation.background_audio["vocals.wav"].replace(
-            "vocals", "vocals_enhanced"
-        )
-        speaker = self.create_speaker(clean_audio_speaker)
+        clean_audio_speaker = video_translation.background_audio["vocals.wav"].replace("vocals", "vocals_enhanced")
+        speaker = self.create_speaker(clean_audio_speaker)    
 
-        for idx, segment in enumerate(
-            tqdm(
-                video_translation.translated_texts,
-                desc="Voice conversion pipeline.",
-                leave=True,
-            )
-        ):
+        for idx, segment in enumerate(tqdm(video_translation.translated_texts, desc='Voice conversion pipeline.', leave=True)):            
             folder_path, audio_name = os.path.split(segment.source_file)
             audio_save_path = os.path.join(temp_folder, audio_name)
-
+            
             self.tone_color_converter.convert(
                 audio_src_path=segment.generated_file,
                 src_se=speaker,
                 tgt_se=speaker,
-                output_path=audio_save_path,
+                output_path=audio_save_path
             )
-
+        
             video_translation.translated_texts[idx].generated_file = audio_save_path
         return video_translation
 
 
 class XTTSClient:
-    def __init__(
-        self,
-        file_repository: FileRepository,
-        tts_model_id="tts_models/multilingual/multi-dataset/xtts_v2",
-        device="cuda",
-    ):
+    def __init__(self,
+                file_repository: FileRepository,
+                tts_model_path="./models_weights/xtts_model/tts_models/multilingual/multi-dataset/xtts_v2",
+                device="cuda"):
         gpu = True if device == "cuda" else False
-        print(os.path.abspath(tts_model_id))
-        self.tts = TTS(tts_model_id, gpu=gpu)
-        self._file_repository = file_repository
+        tts_model_path = os.path.abspath(tts_model_path)
 
-    def generate_audio(
-        self, text: str, source_audio_path: str, save_path: str, language: str
-    ):
+        config_path = "./models_weights/xtts_model/tts_models/multilingual/multi-dataset/xtts_v2/config.json"
+        config_path = os.path.abspath(config_path)
+        self.tts = TTS(model_path=tts_model_path, config_path=config_path)
+        self._file_repository = file_repository
+    
+    def generate_audio(self, text: str, source_audio_path: str, save_path: str, language: str):
         """
         Generates and styles audio, saves according to the path.
         : param style: whether voice conversion should be applied
         """
-        self.tts.tts_to_file(
-            text=text,
-            file_path=save_path,
-            speaker_wav=source_audio_path,
-            language=language,
-            enable_text_splitting=False,
-            repetition_penalty=2.0,
-        )
+        self.tts.tts_to_file(text=text, file_path=save_path, speaker_wav=source_audio_path, language=language, enable_text_splitting=False, repetition_penalty=2.0)
 
     def tts_pipeline(self, video_translation, temp_folder, language="en"):
         language = map_language_to_code(language, "whisper")
 
-        for idx, segment in enumerate(
-            tqdm(
-                video_translation.translated_texts,
-                desc="Voice generation pipeline.",
-                leave=True,
-            )
-        ):
-            file_path = os.path.join(temp_folder, f"{segment.start}_{segment.end}.wav")
+        for idx, segment in enumerate(tqdm(video_translation.translated_texts, desc='Voice generation pipeline.', leave=True)):
+            file_path = os.path.join(temp_folder, f"{segment.start}_{segment.end}.wav")            
 
             if not os.path.exists(file_path):
                 add_pauses(segment.source_file)
                 self.generate_audio(
-                    segment.translation, segment.source_file, file_path, language
-                )
+                                    segment.translation,
+                                    segment.source_file,
+                                    file_path,
+                                    language
+                                    )
             video_translation.translated_texts[idx].generated_file = file_path
         return video_translation
--- src/ml/text_to_speech_service/video_dubbing_manager.py
+++ src/ml/text_to_speech_service/video_dubbing_manager.py
@@ -19,43 +19,35 @@ class VideoDubbingManager:
         self.logger = logger
 
         self.model_vad = load_silero_vad()
-
+    
     def get_pause(self, wav_path, sr, seconds=True):
         wav = read_audio(wav_path, sampling_rate=sr)
-        speech_timestamps = get_speech_timestamps(
-            wav, self.model_vad, return_seconds=seconds
-        )
+        speech_timestamps = get_speech_timestamps(wav, self.model_vad, return_seconds=seconds)
 
-        speech_timestamps[0]["pause"] = 0
+        speech_timestamps[0]['pause'] = 0
         for t_idx in range(1, len(speech_timestamps)):
-            speech_timestamps[t_idx]["pause"] = (
-                speech_timestamps[t_idx]["start"] - speech_timestamps[t_idx - 1]["end"]
-            )
+            speech_timestamps[t_idx]['pause'] = speech_timestamps[t_idx]['start'] - speech_timestamps[t_idx - 1]['end']
 
-        pause_long = sum([i["pause"] for i in speech_timestamps])
+        pause_long = sum([i['pause'] for i in speech_timestamps])
         return pause_long, speech_timestamps
-
+    
     def merge_pauses(self, wav_path, sr, timestamps):
         wav = read_audio(wav_path, sampling_rate=sr)
 
-        audio_samples = [
-            wav[timestamps[0]["start"] : timestamps[0]["end"] + 1].unsqueeze(0)
-        ]
+        audio_samples = [wav[timestamps[0]['start']: timestamps[0]['end'] + 1].unsqueeze(0)]
 
         for audio_info in timestamps[1:]:
-            pause = torch.zeros((1, int(audio_info["pause"])))
+            pause = torch.zeros((1, int(audio_info['pause'])))
             audio_samples.append(pause)
 
-            audio = wav[audio_info["start"] : audio_info["end"] + 1].unsqueeze(0)
+            audio = wav[audio_info['start']: audio_info['end'] + 1].unsqueeze(0)
             audio_samples.append(audio)
-
+        
         audio_final = torch.cat(audio_samples, dim=1)
-        self.logger.file_logger.info(
-            f"Changed audio length is from {wav.shape[0] / sr} to {audio_final.shape[1] / sr}"
-        )
+        self.logger.file_logger.info(f"Changed audio length is from {wav.shape[0] / sr} to {audio_final.shape[1] / sr}")
         torchaudio.save(wav_path, audio_final, sr)
         return audio_final
-
+    
     def change_pauses(
         self, wav_gen_path: str, wav_source_path: str, sr_gen: int, sr_source: int
     ):
@@ -95,88 +87,78 @@ class VideoDubbingManager:
         audio = self.merge_pauses(wav_gen_path, sr_gen, timestamps_gen)
         return audio
 
+    
+
     def merge_timestamps_speedup(self, df, video_length, source_sample_rate):
         """
         Algorithm that works on time stretching - not the best one.
         """
-        df["gen_dur"] = df["styled_generated_path"].apply(
-            lambda audio_path: self.get_audio_length(audio_path)
-        )
-        df["pause"] = (
-            df["start"].shift(-1) - df["end"]
-        )  # пауза между двумя предложениями
-        df["pause"] = df["pause"].fillna(0)
-        df["dur_gen_pause"] = (
-            df["gen_dur"] + df["pause"]
-        )  # длина сгенерированной речи + пауза, которую можно сделать
-        df["duration_orig"] = df["end"] - df["start"]
-        df["speed_rate"] = df["dur_gen_pause"] / df["duration_orig"]
+        df['gen_dur'] = df['styled_generated_path'].apply(lambda audio_path: self.get_audio_length(audio_path))
+        df['pause'] = df['start'].shift(-1) - df['end'] # пауза между двумя предложениями 
+        df['pause'] = df['pause'].fillna(0)
+        df['dur_gen_pause'] = df['gen_dur'] + df['pause'] # длина сгенерированной речи + пауза, которую можно сделать 
+        df['duration_orig'] = df['end'] - df['start']
+        df['speed_rate'] = df['dur_gen_pause'] / df['duration_orig']
         full_audio_blank = torch.zeros((1, int(video_length * source_sample_rate)))
 
         for i, line in df.iterrows():
             audio, sr = torchaudio.load(line.styled_generated_path)
 
             start = line.start
-
+                    
             audio = time_stretch(audio.squeeze().numpy(), sr, rate=line.speed_rate)
             audio = torch.tensor(audio).unsqueeze(0)
 
-            start_pos = int(start * source_sample_rate)
+            start_pos = int(start*source_sample_rate)
             end_pos = int(start_pos + audio.shape[-1])
-
-            full_audio_blank[0, int(start_pos) : int(end_pos)] = audio[0]
+            
+            full_audio_blank[0, int(start_pos): int(end_pos)] = audio[0]
 
         return full_audio_blank
-
+    
     def merge_timestamps_pause_based(self, video_translation, vocals_audio):
         """
         Algorithm that works on filling the pauses - not the best one.
         """
         df = pd.DataFrame()
-        df["start"] = [segment.start for segment in video_translation.translated_texts]
-        df["end"] = [segment.end for segment in video_translation.translated_texts]
+        df["start"] = [segment.start for segment in video_translation.translated_texts] 
+        df["end"] = [segment.end for segment in video_translation.translated_texts] 
         df["pause"] = df["start"].shift(-1) - df["end"]
-        df["gen_dur"] = [
-            torchaudio.load(segment.generated_file)[0].shape[1] / 24000
-            for segment in video_translation.translated_texts
-        ]
-        df["pause"] = df["start"].shift(-1) - df["end"]
-        df["dur_gen_pause"] = df["gen_dur"] + df["pause"]
-        df["place_gen"] = df["end"] - df["start"] + df["pause"]
-        df["gen_end"] = df["start"] + df["gen_dur"]
-        df["can_start"] = [0] + df["gen_end"].to_list()[:-1]
-        df["need_time"] = df["gen_dur"] - df["place_gen"]
-        df["new_start"] = df.apply(
-            lambda x: x.start - x.need_time if x.need_time > 0 else x.start, axis=1
-        )
-        df["need_speedup"] = df["gen_dur"] > df["place_gen"]
-        df["duration_orig"] = df["end"] - df["start"]
+        df['gen_dur'] = [torchaudio.load(segment.generated_file)[0].shape[1] / 24000 for segment in video_translation.translated_texts]
+        df['pause'] = df['start'].shift(-1) - df['end']
+        df['dur_gen_pause'] = df['gen_dur'] + df['pause']
+        df['place_gen'] = df['end'] - df['start'] + df['pause']
+        df['gen_end'] = df['start'] + df['gen_dur']
+        df['can_start'] = [0] + df['gen_end'].to_list()[:-1]
+        df['need_time'] = df['gen_dur'] - df['place_gen']
+        df['new_start'] = df.apply(lambda x: x.start - x.need_time if x.need_time > 0 else x.start, axis=1)
+        df['need_speedup'] = df['gen_dur'] > df['place_gen']
+        df['duration_orig'] = df['end'] - df['start']
 
         prev_audio, sr = torchaudio.load(vocals_audio)
-        prev_audio_shape = prev_audio.shape[1]
+        prev_audio_shape = prev_audio.shape[1]        
         audio_generated = torch.zeros((1, int(prev_audio_shape)))
 
         for idx, segment in enumerate(video_translation.translated_texts):
             audio, sr = torchaudio.load(segment.generated_file)
-            start_pos = df.loc[idx, "new_start"] * sr
+            start_pos = df.loc[idx, 'new_start'] * sr
 
             start_pos = np.ceil(start_pos)
             end_pos = start_pos + audio.shape[-1]
             end_pos = np.ceil(end_pos)
 
-            audio_generated[0, int(start_pos) : int(end_pos)] = audio[0]
+            audio_generated[0, int(start_pos): int(end_pos)] = audio[0]
 
         return audio_generated, sr
+    
 
     def merge_timestamps_stretch_whole(self, video_translation, vocals_audio):
         df = pd.DataFrame()
-        df["start"] = [segment.start for segment in video_translation.translated_texts]
+        df["start"] = [segment.start for segment in video_translation.translated_texts] 
         df["end"] = [segment.end for segment in video_translation.translated_texts]
-        df["texts"] = [
-            segment.translation for segment in video_translation.translated_texts
-        ]
+        df["texts"] = [segment.translation for segment in video_translation.translated_texts]
 
-        df["pause"] = df["start"].shift(-1) - df["end"]  # pause between two samples
+        df["pause"] = df["start"].shift(-1) - df["end"] # pause between two samples 
 
         df["generated_audio_length"] = [0] * df.shape[0]
         df["generated_audio_pause"] = [0] * df.shape[0]
@@ -188,18 +170,12 @@ class VideoDubbingManager:
             # calculate the last pause length
             source_audio, source_sr = torchaudio.load(vocals_audio)
             target_audio_length = source_audio.shape[1] / source_sr
-            df.loc[df.shape[0] - 1, "pause"] = (
-                target_audio_length - df.loc[df.shape[0] - 1, "end"]
-            )  # the last pause
+            df.loc[df.shape[0] - 1, "pause"] = target_audio_length - df.loc[df.shape[0] - 1, "end"] # the last pause
         elif df.shape[0] == 1:
             df.loc[0, "pause"] = 0
 
-        audio_first, sr_generated = torchaudio.load(
-            video_translation.translated_texts[0].generated_file
-        )
-        previous_pause = torch.zeros(
-            (1, int(video_translation.translated_texts[0].start * sr_generated))
-        )
+        audio_first, sr_generated = torchaudio.load(video_translation.translated_texts[0].generated_file)
+        previous_pause = torch.zeros((1, int(video_translation.translated_texts[0].start * sr_generated)))
 
         audio_generated = previous_pause
 
@@ -207,12 +183,8 @@ class VideoDubbingManager:
 
         for idx, segment in enumerate(video_translation.translated_texts):
             audio, sr_generated = torchaudio.load(segment.generated_file)
-            audio = self.change_pauses(
-                segment.generated_file,
-                segment.source_file,
-                sr_gen=sr_generated,
-                sr_source=source_sr,
-            )
+            audio = self.change_pauses(segment.generated_file, segment.source_file,
+                                    sr_gen=sr_generated, sr_source=source_sr)
 
             audio_length = audio.shape[1] / sr_generated
 
@@ -220,7 +192,7 @@ class VideoDubbingManager:
             time_dif = df["source_length"].iloc[idx] - audio_length
 
             df.loc[idx, "time_dif"] = time_dif
-
+            
             if time_dif < 0:
                 # TODO: check that it is not negative (!)
                 df.loc[idx, "pause"] -= time_dif
@@ -235,15 +207,13 @@ class VideoDubbingManager:
             audio_generated = torch.cat((audio_generated, audio, pause), dim=1)
 
         generated_audio_length = audio_generated.shape[1] / sr_generated
-        speed_up_rate = generated_audio_length / target_audio_length
+        speed_up_rate = generated_audio_length/target_audio_length
 
         self.logger.file_logger.info(f"Rate for speed up is {speed_up_rate}")
         if speed_up_rate > 1:
-            audio_generated = time_stretch(
-                audio_generated.squeeze().numpy(), sr=sr_generated, rate=speed_up_rate
-            )
+            audio_generated = time_stretch(audio_generated.squeeze().numpy(), sr=sr_generated, rate=speed_up_rate)
             audio_generated = torch.tensor(audio_generated).unsqueeze(0)
 
-        data_json = df.to_dict("records")
+        data_json = df.to_dict('records')
         self.logger.log_json(data=data_json, file_name="pauses_for_stretch_whole.json")
         return audio_generated, sr_generated
