62be889994d8fa54af5219bf2d0e450391331c93
MilanaShhanukova <milana.shxanukova15@gmail.com>
2024-10-16 15:56:48 +0300

    add the support for asr part

--- src/ml/speech_to_text_service/asr_client.py
+++ src/ml/speech_to_text_service/asr_client.py
@@ -1,48 +1,31 @@
-from time import sleep
-
 import cattrs
-import requests
-import whisper
-import whisperx
-import attr
 
+import sys
+
+sys.path.append("/app")
+sys.path.append("/app/whisperX")
+sys.path.append("/app/src")
+
+from whisperX import whisperx
+import attr
+import torch
+import os
+import requests
+from time import sleep
 from src.utils.ml_processing.lang2code_mapper import map_language_to_code
 
-# @attr.s(auto_attribs=True)
-# class WordTimestamp:
-#     start: float
-#     end: float
-#     word: str
+torch.backends.cuda.matmul.allow_tf32 = True
+torch.backends.cudnn.allow_tf32 = True
 
-# @attr.s(auto_attribs=True)
-# class Segment:
-#     avg_logprob: float
-#     compression_ratio: float
-#     end: float
-#     id: int
-#     no_speech_prob: float
-#     seek: int
-#     start: float
-#     temperature: int
-#     text: str
-#     tokens: list[int]
-#     word_timestamps: list[WordTimestamp] = attr.ib(factory=list)
-#     speaker: str = None 
 
 @attr.s(auto_attribs=True)
 class Segment:
-    # avg_logprob: float
-    # compression_ratio: float
     end: float
-    # id: int
-    # no_speech_prob: float
-    # seek: int
     start: float
-    # temperature: int
     text: str
     words: list[dict]
-    # word_timestamps: list[WordTimestamp] = attr.ib(factory=list)
-    speaker: str = None 
+    speaker: str = None
+
 
 @attr.s(auto_attribs=True)
 class Output:
@@ -52,107 +35,16 @@ class Output:
     transcription: str
     translation: str = None  # Optional, since it might be null
     segments: list[Segment] = attr.ib(factory=list)
-    # word_timestamps: list[WordTimestamp] = attr.ib(factory=list)
+
 
 @attr.s(auto_attribs=True)
 class TranscriptionData:
-    # delayTime: int
-    # executionTime: int
-    # id: str
     output: Output
-    # status: str
+
 
 @attr.s(auto_attribs=True)
 class TranscriptionDataLocal:
     output: Output
-
-class ASRX:
-
-    token: str
-
-    def __init__(self, device) -> None:
-        compute_type = "float32" if device == "cpu" else "float16"
-        self.model = whisperx.load_model("large-v2", device, compute_type=compute_type)
-
-        token = "***REDACTED-HF-TOKEN***"
-        self.diarize_model = whisperx.DiarizationPipeline(use_auth_token=token, device=device)
-
-        self.device = device
-
-    def transcribe(self, source_file: str, lang='en') -> Output:
-        audio = whisperx.load_audio(source_file)
-        response = self.model.transcribe(audio, batch_size=8)
-
-        language = response['language']
-
-        model_a, metadata = whisperx.load_align_model(language_code=response["language"], device=self.device)
-
-        response = whisperx.align(response["segments"], model_a, metadata, audio, self.device, return_char_alignments=False)
-
-        diarize_segments = self.diarize_model(audio)
-        response = whisperx.assign_word_speakers(diarize_segments, response)
-
-        all_words = []
-        result = {}
-
-        final_response = {}
-
-        for i in range(len(response['segments'])):
-            all_words.extend([i for i in response['segments'][i]['words']])
-        
-        full_text = " ".join([i['word'] for i in all_words])
-
-        final_response['detected_language'] = language
-        final_response['device'] = 'cuda'
-        final_response['model'] = 'base'
-        final_response['transcription'] = full_text
-        final_response['translation'] = ''
-        final_response['segments'] = response['segments']
-        final_response['word_timestamps'] = all_words
-
-        # result['delayTime'] = 1
-        # result['executionTime'] = 1
-        # result['id'] = 3
-        result['status'] = 'finished'
-
-        result['output'] = final_response
-        return cattrs.structure(result, TranscriptionData).output
-
-class ASRClientFaster:
-
-    token: str
-
-    def __init__(self, api_key: str):
-        self.token = api_key
-
-        self.model = whisper.load_model("medium")
-
-    def transcribe(self, source_file: str, lang: str) -> Output:
-        response = self.model.transcribe(source_file, word_timestamps=True)
-        # response = self.model.transcribe(source_file, word_timestamps=True)
-
-        all_words = []
-
-        result = {}
-
-        for i in range(len(response['segments'])):
-            all_words.extend(response['segments'][i]['words'])
-
-        response['detected_language'] = lang
-        response['device'] = 'cuda'
-        response['model'] = 'base'
-        response['transcription'] = response["text"]
-        response['translation'] = ''
-        response['segments'] = response['segments']
-        response['word_timestamps'] = all_words
-
-        result['delayTime'] = 1
-        result['executionTime'] = 1
-        result['id'] = 3
-        result['status'] = 'finished'
-
-        result['output'] = response
-        return cattrs.structure(result, TranscriptionData).output
 
 
 class ASRClient:
@@ -189,9 +81,9 @@ class ASRClient:
                 "compression_ratio_threshold": 2.4,
                 "logprob_threshold": -1,
                 "no_speech_threshold": 0.6,
-                "word_timestamps": True
+                "word_timestamps": True,
             },
-            "enable_vad": False
+            "enable_vad": False,
         }
 
         response = requests.post(url, json=payload, headers=headers)
@@ -201,10 +93,80 @@ class ASRClient:
             return cattrs.structure(response, TranscriptionData).output
         except Exception:
             print(response)
-            if 'error' in response and response['error'] == 'failed to add to queue':
-                print('Retryable error, retrying...')
+            if "error" in response and response["error"] == "failed to add to queue":
+                print("Retryable error, retrying...")
                 sleep(5)
                 self.transcribe(source_url)
             raise
 
 
+class ASRX:
+
+    token: str
+
+    def __init__(self, device) -> None:
+        compute_type = "float32" if device == "cpu" else "float16"
+        model_path = (
+            "./models_weights/whisper-large-v2/f0fe81560cb8b68660e564f55dd99207059c092e"
+        )
+        model_path = os.path.abspath(model_path)
+
+        self.model = whisperx.load_model(
+            model_path, device=device, compute_type=compute_type, local_files_only=True
+        )
+
+        token = "***REDACTED-HF-TOKEN***"  # secret, move somewhere
+        model_path_diarization = "./models_weights/pyannote/models--pyannote--speaker-diarization-3.1/snapshots/84fd25912480287da0247647c3d2b4853cb3ee5d/config.yaml"
+        model_path_diarization = os.path.abspath(model_path_diarization)
+
+        self.diarize_model = whisperx.DiarizationPipeline(
+            model_path_diarization, use_auth_token=token, device=device
+        )
+
+        self.device = device
+
+    def transcribe(self, source_file: str, lang="en") -> Output:
+        language = map_language_to_code(lang, system="whisper")
+        audio = whisperx.load_audio(source_file)
+        response = self.model.transcribe(audio, language=language, batch_size=8)
+
+        language = response["language"]
+
+        model_a, metadata = whisperx.load_align_model(
+            language_code=response["language"], device=self.device
+        )
+
+        response = whisperx.align(
+            response["segments"],
+            model_a,
+            metadata,
+            audio,
+            self.device,
+            return_char_alignments=False,
+        )
+
+        diarize_segments = self.diarize_model(audio)
+        response = whisperx.assign_word_speakers(diarize_segments, response)
+
+        all_words = []
+        result = {}
+
+        final_response = {}
+
+        for i in range(len(response["segments"])):
+            all_words.extend([i for i in response["segments"][i]["words"]])
+
+        full_text = " ".join([i["word"] for i in all_words])
+
+        final_response["detected_language"] = language
+        final_response["device"] = "cuda"
+        final_response["model"] = "base"
+        final_response["transcription"] = full_text
+        final_response["translation"] = ""
+        final_response["segments"] = response["segments"]
+        final_response["word_timestamps"] = all_words
+
+        result["status"] = "finished"
+
+        result["output"] = final_response
+        return cattrs.structure(result, TranscriptionData).output
